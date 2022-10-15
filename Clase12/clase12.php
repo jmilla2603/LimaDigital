a5a7ae11ae98f132ea5aa099895ceaa57d35c80b
Juan Carlos <jmilla2603@gmail.com>
2022-10-15 14:58:57 -0500

    Clase 12

--- Clase12/clase12.php
+++ Clase12/clase12.php
@@ -1,5 +1,14 @@
 <?php
+    include_once "claseFactura.php";// NATURAL(10) - JURIDICA(20)
+    include_once "claseProductos.php";
 
-    echo "PHP";
+    $documento=new Factura("Juan Milla","15/10/2022","10405108203","976-226451","CONTADO",100.00,18.00,118.00);
 
+    $listaProductos=new Products;
+
+    echo $documento->setMensaje("Factura Ok");
+
+    echo $documento->ImprimirDocumento($listaProductos->ListarProductos());
+
+    echo $documento->getMensaje();
 ?>
---
+++ Clase12/claseProductos.php
@@ -0,0 +1,31 @@
+<?php
+
+class Products{
+    private $lista_productos;
+
+    function __construct()
+    {
+        $this->lista_productos=null;
+    }
+
+    public function ListarProductos():array{
+        return $this->lista_productos=
+            array(array(
+                    "Descripcion"=>"Billetera",
+                    "Precio"=>"15.00",
+                    "Cantidad"=>"2"
+                ),
+                array(
+                    "Descripcion"=>"Mochila",
+                    "Precio"=>"45.80",
+                    "Cantidad"=>"1"
+                ),
+                array(
+                    "Descripcion"=>"Libro - PHP",
+                    "Precio"=>"65.00",
+                    "Cantidad"=>"1"
+                )
+                );
+    }
+}
+?>
