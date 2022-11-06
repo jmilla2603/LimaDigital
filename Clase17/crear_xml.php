aba46731ec35475334e6ae2fb40ee7186406ea87
Juan Milla <jmilla2603@gmail.com>
2022-11-05 19:08:44 -0500

    clase Ok

---
+++ Clase17/crear_xml.php
@@ -0,0 +1,30 @@
+<?php
+
+    $doc=new DOMDocument();
+    $doc->formatOutput=FALSE;
+    $doc->preserveWhiteSpace=TRUE;
+    $doc->encoding='utf-8';
+
+    // EMPieza la creacion del XML
+
+    $productos=array("Lavadora","Televisor","Tostadora");
+
+    $xml='<?xml version="1.0" encoding="UTF-8"?>
+    <factura>
+    <serie>F001</serie>
+    <correlativo>12345678</correlativo>
+    <cliente>
+        <nrodoc tipo="RUC">12345678911</nrodoc>
+        <direccion>dshasjkdhaskjdhsa</direccion>
+        <telefono>1231456</telefono>
+    </cliente>';
+    
+    foreach($productos as $lista){
+        $xml.='<producto>' . $lista .'</producto>';
+    }
+
+    $xml.='</factura>';
+
+$doc->loadXML($xml);
+$doc->save('FACTURA_DEMO.xml');
+?>
